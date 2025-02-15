0e98a1b0e0f25ed8b08138f0ae3e128e0dd9900b
Chrzonit <chrzonir@gmail.com>
2025-02-15 21:14:23 +0100

    Can now get rss and create pdf from the html content

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import feedparser
+import pprint
+import streamlit as st
+from xhtml2pdf import pisa
+feed_urls = [{'feed': 'Dwarkesh', 'url': 'https://www.dwarkeshpatel.com/feed'}]
+
+parsed_feed = feedparser.parse(feed_urls[0]['url'])
+print(type(parsed_feed))
+print(parsed_feed.keys())
+print(parsed_feed['feed'])
+print(parsed_feed['entries'][0].keys())
+print(len(parsed_feed['entries']))
+print(parsed_feed['namespaces'])
+
+pprint.pprint(parsed_feed['entries'][0].content[0].value)
+
+with open('output.pdf', 'wb') as f:
+    pisa.CreatePDF(parsed_feed['entries'][0].content[0].value, f)
+    
