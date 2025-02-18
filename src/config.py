bb1861c5294aa1d0b4d29d7098f734bc3e894081
Max <125830869+Chrzonit@users.noreply.github.com>
2025-02-18 13:10:16 +0100

    Changed URLs and attained token for the .env

--- src/config.py
+++ src/config.py
@@ -1,8 +1,11 @@
 from uuid import uuid4
 
+
+# Remarkable Stuff
 REGISTER_DEVICE_URL = "https://webapp.cloud.remarkable.com/token/json/2/device/new"
-REFRESH_TOKEN_URL = "https://webapp.cloud.remarkable.com/token/json/2/user/new"
-ONE_TIME_CODE = "qvffibis"
+REFRESH_TOKEN_URL = "https://webapp-prod.cloud.remarkable.engineering/token/json/2/user/new"
+FILE_LIST_URL = "https://webapp-prod.cloud.remarkable.engineering/sync/v4/root"
+ONE_TIME_CODE = "vlmqbvgq"
 DEVICE_ID = str(uuid4())
 DEVICE_DESC = "desktop-windows"
 
--- src/main.py
+++ src/main.py
@@ -10,20 +10,17 @@ from .remarkable.api import reMarkable
 feed_urls = [{'feed': 'Dwarkesh', 'url': 'https://www.dwarkeshpatel.com/feed'}]
 
 parsed_feed = feedparser.parse(feed_urls[0]['url'])
-print(type(parsed_feed))
-print(parsed_feed.keys())
-print(parsed_feed['feed'])
-print(parsed_feed['entries'][0].keys())
-print(len(parsed_feed['entries']))
-print(parsed_feed['namespaces'])
 
 pprint.pprint(parsed_feed['entries'][0].content[0].value)
 
 with open('output.pdf', 'wb') as f:
     pisa.CreatePDF(parsed_feed['entries'][0].content[0].value, f)
     
-load_dotenv()
+    
+remarkable = reMarkable()
 
-remarkable = reMarkable(device_token=os.getenv('DEVICE_TOKEN'), user_token=os.getenv('USER_TOKEN'))
+remarkable.get_device_token()
+remarkable.refresh_user_token()
 
-remarkable.test_auth()
+with open('.env', 'w') as file:
+    file.write(f"DEVICE_TOKEN = {remarkable.device_token}\nUSER_TOKEN = {remarkable.user_token}") 
